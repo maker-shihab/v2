a206c529d4470e67d2352bd84c630e87a7ff3150
Shihab <73121069+maker-shihab@users.noreply.github.com>
2021-11-27 16:02:22 +0600

    requer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,32 @@ $(document).ready(function () {
         $('.header').fadeOut('headActive');
     });
 });
+
+$(document).ready(function(){
+    $('.clients_content').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+        {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+        }
+        }
+    ]
+    });
+});
+          
